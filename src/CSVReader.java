0a71922777ac2535308c2bc7aec8b515be10e405
Alan Donoso Naumczuk <adonoso@itba.edu.ar>
2017-05-31 22:50:54 -0300

    Now I'm Iterable ;)

--- src/CSVReader.java
+++ src/CSVReader.java
@@ -3,8 +3,9 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 
-public class CSVReader {
+public class CSVReader implements Iterable<Collection<String>> {
     private String path;
     private String splitChar;
     private RandomAccessFile fileReader;
@@ -27,7 +28,7 @@ public class CSVReader {
     }
 
     /*
-     * Return a collection with the values readed from the line where file reader is pointing to.
+     * Returns a collection with the values readed from the line where file reader is pointing to.
      */
     public Collection<String> getLine() throws IOException {
         Collection<String> values = new ArrayList<>();
@@ -43,15 +44,13 @@ public class CSVReader {
     }
 
     /*
-     * Return a collection with the values readed from the specified line of the file.
+     * Returns a collection with the values readed from the specified line of the file.
      * The line is counted from the beginning of the file, number zero stands for the first line.
      */
     public Collection<String> getLine(long lineNumber) throws IOException {
         Collection<String> values = new ArrayList<>();
 
-        for (long i = 0; i < lineNumber; i++) {
-            fileReader.readLine();
-        }
+        fileReader.seek(lineNumber);
 
         String line = fileReader.readLine();
 
@@ -66,4 +65,63 @@ public class CSVReader {
         return values;
     }
 
+    /*
+     * Returns false if there are more lines to read. Otherwise return true.
+     */
+    public boolean feof() throws IOException {
+        boolean isEof = true;
+        long lastPointer = fileReader.getFilePointer();
+
+        if(fileReader.readLine() != null)
+            isEof = false;
+
+        fileReader.seek(lastPointer);
+
+        return isEof;
+    }
+
+    /*
+     * Returns our CSV Custom Iterator
+     */
+    @Override
+    public Iterator<Collection<String>> iterator() {
+        return new CSVIterator();
+    }
+
+    class CSVIterator implements Iterator<Collection<String>> {
+        protected CSVIterator()
+        {
+            try {
+                resetFilePointer();
+            }
+            catch (IOException e) {
+                throw new RuntimeException(e.getMessage());
+            }
+        }
+
+        @Override
+        public boolean hasNext() {
+            try {
+                boolean hasNext = !feof();
+
+                if(!hasNext)
+                    resetFilePointer();
+
+                return hasNext;
+            }
+            catch (IOException e) {
+                throw new RuntimeException(e.getMessage());
+            }
+        }
+
+        @Override
+        public Collection<String> next() {
+            try {
+                return getLine();
+            }
+            catch (IOException e) {
+                throw new RuntimeException(e.getMessage());
+            }
+        }
+    }
 }
