f1d5f792fc3b72eb3d661eb2b12c4a8b2e4fd05e
Alan Donoso Naumczuk <adonoso@itba.edu.ar>
2017-06-03 16:01:25 -0300

    Update OutpuLayer.java

--- src/Model/NeuralNetwork/OutpuLayer.java
+++ src/Model/NeuralNetwork/OutpuLayer.java
@@ -2,9 +2,6 @@ package Model.NeuralNetwork;
 
 import org.nd4j.linalg.api.ndarray.INDArray;
 
-/**
- * Created by SB on 29/05/2017.
- */
 public class OutpuLayer extends Layer {
 
     public OutpuLayer(int input, int output, Activation.ACTIVATION function) {
