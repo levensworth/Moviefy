4236d07b9d9a55d626af7851b4a916675f5ae7e6
alejoaquili <aaquili@itba.edu.ar>
2017-06-05 23:55:07 -0300

    Query Documentation

--- src/Model/Query.java
+++ src/Model/Query.java
@@ -3,30 +3,25 @@ package Model;
 import java.util.ArrayList;
 import java.util.List;
 
+/** The {@code Query} class represents the criteria pass by the user to filter {@code Movie} of a database.
+ *
+ * @author Bassani, Santiago
+ * @version 1.0
+ */
+
 public class Query {
-   /*
-    * this class contains the params
-    * that the user wants in a movie
-    *
-    *
-    */
 
     private int minYear;
     private int maxYear;
     private List<String> genres;
     private List<Actor> actors;
 
-    public int getMinYear() {
-        return minYear;
-    }
-
-    public int getMaxYear() {
-        return maxYear;
-    }
-
-    public List<String> getGenres() {
-        return genres;
-    }
+    /**Creates a {@code Query} with specified range of year, genres and specified actors.
+     * @param minYear The Query's bottom of the years range.
+     * @param maxYear The Query's top of the years range.
+     * @param genres The Query's genres.
+     * @param actors The Query's actors.
+     */
 
     public Query(int minYear, int maxYear, ArrayList<String> genres, ArrayList<Actor> actors) {
 
@@ -35,6 +30,9 @@ public class Query {
         this.genres = genres;
         this.actors = actors;
     }
+
+    /**Creates a {@code Query} with the range of year, genres and actors initialized with default values.
+     */
 
     public Query() {
         minYear = 0;
@@ -63,7 +61,22 @@ public class Query {
         return this;
     }
 
+    public int getMinYear() {
+        return minYear;
+    }
 
+    public int getMaxYear() {
+        return maxYear;
+    }
+
+    public List<String> getGenres() {
+        return genres;
+    }
+
+    /** Returns true if the movie fulfill the specified requirements of the {@code Query}
+     * @param  movie a specified {@code Movie}.
+     *
+     */
 
     public boolean validate(Movie movie) {
         if (!(movie.getYear() >= minYear && movie.getYear() <= maxYear)) {
