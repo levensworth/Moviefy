afaa847730e0b2137c92757efa5a5a95579637e5
alejoaquili <aaquili@itba.edu.ar>
2017-06-06 19:50:46 -0300

    Model Documentation

--- src/Model/Actor.java
+++ src/Model/Actor.java
@@ -3,12 +3,27 @@ package Model;
 import java.util.Collection;
 import java.util.List;
 
+/** The {@code Actor} class represents an actor in this system.
+ *
+ * @author Donoso Naumczuk, Alan Nicolas
+ * @version 1.0
+ */
+
 public class Actor extends Person {
 
+    /** Creates an {@code Actor} with a specified ID number name and {@code Application}.
+     * @param id The actor ID.
+     * @param name The actor name.
+     * @param app The specified {@code Application} for the {@code Actor}.
+     */
 
     public Actor(long id, String name, Application app) {
         super(id, name, app);
     }
+
+    /** This method ask the actor's specified {@code Application} to get all movies in which the person is actor.
+     * @return a {@code Collection} of movies acted by this.
+     */
 
     public Collection<Movie> getMovies() {
         return getApp().getMovies(this);
--- src/Model/Director.java
+++ src/Model/Director.java
@@ -4,7 +4,20 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+/** The {@code Director} class represents an director in this system.
+ *
+ * @author Donoso Naumczuk, Alan Nicolas
+ * @version 1.0
+ */
+
 public class Director extends Person{
+
+    /** Creates an {@code Director} with a specified ID number name and {@code Application}.
+     * @param id The director ID.
+     * @param name The director name.
+     * @param app The specified {@code Application} for the {@code Director}.
+     */
+
     public Director(long id, String name, Application app) {
         super(id, name, app);
     }
@@ -12,8 +25,12 @@ public class Director extends Person{
     //i belive this method should not be implemented in actor nor director
     //cause , the way  i define the parameters of get movie makes it return the right information
     //as the argument @this will change depending of the object from which it was fired
+
+    /** This method ask the director's specified {@code Application} to get all movies in which the person is director.
+      * @return a {@code Collection} of movies directed by this.
+     */
+
     public Collection<Movie> getMovies() {
-        //this method ask the app to get all movies in which the person is a director
         return getApp().getMovies(this);
     }
 
