4f16a27a8b49d3b3eb8e9d92f7f70146a0512ac4
alejoaquili <aaquili@itba.edu.ar>
2017-06-05 23:36:35 -0300

    MovieFeedBack Documentation

--- src/Model/MovieFeedBack.java
+++ src/Model/MovieFeedBack.java
@@ -1,9 +1,22 @@
 package Model;
 
+/** The {@code MovieFeedBack} class represents a set of values that broadcast the user feedback with a
+ * specified {@code Movie}.
+ *
+ * @author Bassani, Santiago
+ * @version 1.0
+ */
+
 public class MovieFeedBack {
 
     private Movie movie;
+    /**The raiting is an instance variable of type Built-in integer that represents the user punctuation*/
     private int rating;
+
+    /**Creates a {@code MovieFeedBack}, with a specified {@code Movie} and a specified punctuation for this movie.
+     * @param movie The MovieFeedBack {@code Movie}
+     * @param  rating The MovieFeedBack raiting of the movie.
+     */
 
     public MovieFeedBack(Movie movie, int rating) {
         this.movie = movie;
