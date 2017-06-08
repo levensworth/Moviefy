d28ef86948d9648ac85c08146d8391a0353dcb4d
Ignacio Nicolás Negro Caino <nachonegrocaino@gmail.com>
2017-06-07 22:31:01 -0300

    there isnt plan B.

---
+++ src/ThereIsntPlanB/main.java
@@ -0,0 +1,12 @@
+package ThereIsntPlanB;
+
+/**
+ * Created by nacho on 07/06/2017.
+ */
+public class main {
+    public static void main(String[] args) {
+        MyFrame panel = new MyFrame("loopTest",1500);
+        panel.setVisible(true);
+        panel.Ten();
+    }
+}
