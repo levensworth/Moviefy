6d463220cde6ee298a0b4862d33ffa60f3db7c0d
Ignacio Nicolás Negro Caino <nachonegrocaino@gmail.com>
2017-06-18 18:40:16 -0300

    tags and actors added

---
+++ src/GUI/RatingSlider.java
@@ -0,0 +1,20 @@
+package GUI;
+import javax.swing.JSlider;
+import java.awt.Font;
+
+class RatingSlider extends JSlider{
+     private static final int MAX_RATE = 10;
+     private static final int MIN_RATE = 0;
+
+     protected RatingSlider(int x, int y, int width,int height){
+        super(JSlider.HORIZONTAL,MAX_RATE,MIN_RATE);
+        setValue((MAX_RATE-MIN_RATE)/2);
+        setMinorTickSpacing(1);
+        setBounds(x,y,width,height);
+        setPaintLabels(true);
+        setPaintTicks(true);
+        setPaintTrack(true);
+        setFont(new Font(getFont().getName(),Font.BOLD,getWidth()/20));
+        setLabelTable(createStandardLabels(1));
+     }
+}
